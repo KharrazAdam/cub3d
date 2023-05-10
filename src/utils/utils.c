9d2941f76391bd37031c8007706754cc72b84699
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2023-05-10 15:36:17 +0100

    sigf

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: akharraz <akharraz@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 15:39:38 by ysakine           #+#    #+#             */
-/*   Updated: 2023/05/09 22:46:36 by akharraz         ###   ########.fr       */
+/*   Updated: 2023/05/10 15:33:48 by akharraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
-	// if (x < 0 || y >= HIGHT || y < 0)
-	// 	return ;
+	if (x < 0 || y >= HIGHT || y < 0)
+		return ;
 	dst = (char *)data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
